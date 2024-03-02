78c59a098660f9e9b3cefc633ea6dd91c3326026
Marco Bassaletti <bassaletti@gmail.com>
2024-03-02 00:53:57 -0800

    [CMake] Prevent cmake for picking up gcc or the XCode toolchain.
    We need to prepend the PATH of the llvm build as an environment var to
    the build.sh, I'll do that later.

--- calc/build.sh
+++ calc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p build
-cmake -GNinja -DLLVM_DIR=$LLVM_DIR -B build -S .
+cmake -GNinja -DLLVM_DIR=$LLVM_DIR -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -B build -S .
 cmake --build build
